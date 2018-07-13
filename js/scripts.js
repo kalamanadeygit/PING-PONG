64ceea07d644c68dc8dbc8766c384ebde98fcf1d
kalamanadeygit <kalamanadey@gmail.com>
2018-07-13 23:11:48 +0300

    pinpong work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
-$(document).ready function()
-{
-      $("form#formping").submit(function(event)
-      {
-        event.preventDefault();
-        var userinput=$("input#number").val();
-        console.log(userinput);
-        $("p.display").text(userinput);
 
-      })
+var number=function(countTo){
+  var run =[];
+  for (var i=1; i<=countTo; i++){
+  if (i % 15 === 0){
+    run.push('pingpong');
+  }else  if (i % 5 === 0) {
+      run.push('pong');
+    }
+  else if (i %3=== 0) {
+  run.push('ping');
+    }  else {
+    run.push(i);
+  }
+    }
+  return run;
+};
+
+$(document).ready( function(){
+      $("form#form").submit(function(event) {
+        event.preventDefault();
+        $("#display").empty();
+        var countTo=parseInt($("input#countTo").val());
+        var display=number(countTo);
+        display.forEach(function(item){
+        $("#display").append(' <li>' + item + '</li>');
+});
+      });
 });
